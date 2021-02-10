20f7ea679fd3a8f6d24a11016477e77a1209e919
梅木邦彦 <umekikunihiko@umekikunihikonoMacBook-Pro.local>
2021-02-10 19:50:25 +0900

    copy

---
+++ kyuu/database/migrations/2021_02_10_190900_create_shuhsokus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShuhsokusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shushokus', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('bunrui');
+            $table->integer('kakaku');
+            $table->integer('genka');
+            $table->string('name');
+            $table->boolean('display');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shushokus');
+    }
+}
